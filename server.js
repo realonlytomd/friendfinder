fb39aea59f1b91b8a3b8a60f44e0b69dc8477a52
realonlytomd <mclaughlintomd@gmail.com>
2018-01-25 14:52:46 -0600

    in process of adding css

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ var PORT = process.env.PORT || 3000;
 // This sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));02
 app.use(bodyParser.json());
+app.use(express.static("public"));
 
 // This points the server to the route files.
 // They give the server a map of how to respond
